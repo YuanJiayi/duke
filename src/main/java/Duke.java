a8cd6daf3a18242cf4525115351ed83b6e4dda9f
Yuan Jiayi <yuanjiayi2000@gmail.com>
2019-08-28 03:25:36 +0800

    add DukeExceptions Class; improve some format error

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -33,7 +33,7 @@ public class Duke {
             if (in.equals("list")) {
                 System.out.println("__________________________________________________\n");
                 for (int i = 1; dukeList[i-1] != null; i++) {
-                    System.out.println(i + "." + dukeList[i-1].toString() + "\n");
+                    System.out.println("   " + i + "." + dukeList[i-1].toString() + "\n");
                 }
                 System.out.println("__________________________________________________\n");
             }
@@ -43,45 +43,64 @@ public class Duke {
                 int number = Integer.parseInt(temp);
                 dukeList[number - 1].markAsDone();
                 System.out.println("__________________________________________________\n");
-                System.out.println("Nice! I've marked this task as done:\n");
-                System.out.println("[✓]" + dukeList[number - 1].description + "\n");
+                System.out.println("   Nice! I've marked this task as done:\n");
+                System.out.println("   [✓] " + dukeList[number - 1].description + "\n");
                 System.out.println("__________________________________________________\n");
             }
 
             // other commands like "read book" to be added in the list
             else {
-                String[] keyword = in.split(" ");
-                switch (keyword[0]) {
-                    case "todo": {
-                        Task task = new ToDo (in.replaceFirst("todo ", ""));
-                        dukeList[tasknumber++] = task;
-                        System.out.println("__________________________________________________\n");
-                        System.out.println("   Got it. I've added this task: \n");
-                        System.out.println("   " + dukeList[tasknumber-1].toString() + "\n");
-                        System.out.println(" Now you have " + tasknumber + " tasks in the list.\n");
-                        System.out.println("__________________________________________________\n");
-                        break;
+                try {
+                    String[] keyword = in.split(" ");
+                    if (!(keyword[0].equals("todo") || keyword[0].equals("event") || keyword[0].equals("deadline"))) {
+                        throw new DukeExceptions("   \u2639 OOPS!!! I'm sorry, but I don't know what that means :-(\n");
                     }
-                    case "event": {
-                        Task task = new Event (in.replaceFirst("event", ""), in.replaceFirst("/at ", "(at: "));
-                        dukeList[tasknumber++] = task;
-                        System.out.println("__________________________________________________\n");
-                        System.out.println("   Got it. I've added this task: \n");
-                        System.out.println("   " + dukeList[tasknumber-1].toString() + "\n");
-                        System.out.println(" Now you have " + tasknumber + " tasks in the list.\n");
-                        System.out.println("__________________________________________________\n");
-                        break;
+
+
+                    switch (keyword[0]) {
+                        case "todo": {
+                            if (in.equals("todo")) {
+                                throw new DukeExceptions("   \u2639 OOPS!!! The description of a todo cannot be empty.\n");
+                            }
+                            Task task = new ToDo(in.replaceFirst("todo ", ""));
+                            dukeList[tasknumber++] = task;
+                            System.out.println("__________________________________________________\n");
+                            System.out.println("   Got it. I've added this task: \n");
+                            System.out.println("   " + dukeList[tasknumber - 1].toString() + "\n");
+                            System.out.println("   Now you have " + tasknumber + " tasks in the list.\n");
+                            System.out.println("__________________________________________________\n");
+                            break;
+                        }
+                        case "event": {
+                            if (in.equals("event")) {
+                                throw new DukeExceptions("   \u2639 OOPS!!! The description of an event cannot be empty.\n");
+                            }
+                            Task task = new Event(in.replaceFirst("event", ""), in.replaceFirst("/at ", "(at: "));
+                            dukeList[tasknumber++] = task;
+                            System.out.println("__________________________________________________\n");
+                            System.out.println("   Got it. I've added this task: \n");
+                            System.out.println("   " + dukeList[tasknumber - 1].toString() + "\n");
+                            System.out.println("   Now you have " + tasknumber + " tasks in the list.\n");
+                            System.out.println("__________________________________________________\n");
+                            break;
+                        }
+                        case "deadline": {
+                            if (in.equals("deadline")) {
+                                throw new DukeExceptions("   \u2639 OOPS!!! The description of a deadline cannot be empty.\n");
+                            }
+                            Task task = new Deadline(in.replaceFirst("deadline ", ""), in.replaceFirst("/by ", "(by: "));
+                            dukeList[tasknumber++] = task;
+                            System.out.println("__________________________________________________\n");
+                            System.out.println("   Got it. I've added this task: \n");
+                            System.out.println("   " + dukeList[tasknumber - 1].toString() + "\n");
+                            System.out.println("   Now you have " + tasknumber + " tasks in the list.\n");
+                            System.out.println("__________________________________________________\n");
+                            break;
+                        }
                     }
-                    case "deadline": {
-                        Task task = new Deadline(in.replaceFirst("deadline ", ""), in.replaceFirst("/by ", "(by: "));
-                        dukeList[tasknumber++] = task;
-                        System.out.println("__________________________________________________\n");
-                        System.out.println("   Got it. I've added this task: \n");
-                        System.out.println("   " + dukeList[tasknumber-1].toString() + "\n");
-                        System.out.println(" Now you have " + tasknumber + " tasks in the list.\n");
-                        System.out.println("__________________________________________________\n");
-                        break;
-                    }
+                }
+                catch (DukeExceptions ex) {
+                    ex.printStackTrace();
                 }
             }
         }
